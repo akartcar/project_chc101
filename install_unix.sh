14608b4ba00160c7765a8018c4e498c72f73fb1e
akartcar <138850460+akartcar@users.noreply.github.com>
2023-11-20 22:20:47 +0800

    Update install_unix.sh

--- install_unix.sh
+++ install_unix.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -x
 
 # Agent Constants
 PACKAGE_NAME="observiq-otel-collector"
